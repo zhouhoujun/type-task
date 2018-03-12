a07ac04ea6879cdd6d2abf43dca77741560d673f
zhouhoujun <freebirds8281@hotmail.com>
2018-03-12 17:53:17 +0800

    symbols

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import * as symbols from './symbols';
 export const taskSymbols = symbols;
+export const TaskSymbols = symbols;
 export * from './objectUtil';
 export * from './Defer';
 export * from './types';
