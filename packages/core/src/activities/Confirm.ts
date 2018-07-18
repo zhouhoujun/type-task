d8a00a1729d687d58ea679993329cc470fe79c68
houjun <freebirds8281@hotmail.com>
2018-07-19 01:01:20 +0800

    confirm

---
+++ packages/core/src/activities/Confirm.ts
@@ -0,0 +1,75 @@
+import {
+    IActivity, Task, InjectAcitityToken, InjectAcitityBuilderToken,
+    ActivityBuilder, IConfigure, Activity, Expression, ExpressionType
+} from '../core';
+import { Defer, Singleton } from '@ts-ioc/core';
+
+/**
+ * Confirm activity token.
+ */
+export const ConfirmActivityToken = new InjectAcitityToken<ConfirmActivity>('Confirm');
+
+/**
+ * Confirm activity builder token
+ */
+export const ConfirmActivityBuilderToken = new InjectAcitityBuilderToken<ConfirmActivityBuilder>('Confirm');
+
+/**
+ * Confirm activity configure.
+ *
+ * @export
+ * @interface ConfirmConfigure
+ * @extends {IConfigure}
+ */
+export interface ConfirmConfigure extends IConfigure {
+    /**
+     * confirm expression.
+     *
+     * @type {ExpressionType<boolean>}
+     * @memberof ConfirmConfigure
+     */
+    confirm?: ExpressionType<boolean>;
+}
+
+/**
+ * while control activity.
+ *
+ * @export
+ * @class ConfirmActivity
+ * @extends {Activity}
+ */
+@Task(ConfirmActivityToken, ConfirmActivityBuilderToken)
+export class ConfirmActivity extends Activity<any> {
+
+    defer = new Defer<any>();
+    /**
+     * Confirm time.
+     *
+     * @type {Expression<number>}
+     * @memberof ConfirmActivity
+     */
+    confirm: Expression<boolean>;
+
+    async run(data?: any, execute?: IActivity): Promise<any> {
+        let confirm = this.context.exec(this, this.confirm, data);
+        if (confirm) {
+            this.defer.resolve(data);
+        } else {
+            this.defer.reject();
+        }
+        return await this.defer.promise;
+    }
+}
+
+@Singleton(ConfirmActivityBuilderToken)
+export class ConfirmActivityBuilder extends ActivityBuilder {
+
+    async buildStrategy(activity: IActivity, config: ConfirmConfigure): Promise<IActivity> {
+        await super.buildStrategy(activity, config);
+        if (activity instanceof ConfirmActivity) {
+            activity.confirm = await this.toExpression(config.confirm, activity);
+        }
+
+        return activity;
+    }
+}
