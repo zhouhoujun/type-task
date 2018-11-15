07bb8d9805b3a4d5ef60b65a263af76e2258d73a
zhouhoujun <freebirds8281@hotmail.com>
2018-11-15 18:56:49 +0800

    update

--- packages/build/src/core/transform/StreamActivity.ts
+++ packages/build/src/core/transform/StreamActivity.ts
@@ -89,7 +89,9 @@ export class StreamActivity extends NodeActivity {
 
 }
 
-
+/**
+ *  transform context token.
+ */
 export const TransformContextToken = new InjectActivityContextToken(StreamActivity);
 
 /**
